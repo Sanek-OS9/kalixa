05f4221d1121e85105f0bd59277bd258c9961f3f
serhiyosetskiy <serhiy.osetskiy@qbex.io>
2017-12-19 12:54:45 +0200

    edit to seed

--- seed.php
+++ seed.php
@@ -1,33 +1,32 @@
 <?php
 require_once('vendor/autoload.php');
 use app\core\DB;
-use app\models\Payment_method;
+use app\models\{Payment_method,User,Address};
 
 DB::connect();
 
-$payment_method = new Payment_method;
-$payment_method->name = 'ECMC Deposit';
-$payment_method->num = '1';
-$payment_method->num_repeated = '1';
-$payment_method->creationTypeID = '1';
-$payment_method->creationTypeID_repeated = '3';
-$payment_method->refunded = '89';
-$payment_method->save();
+Payment_method::insert([
+  ['name' => 'ECMC Deposit', 'num' => 1, 'num_repeated' => 1, 'creationTypeID' => 1, 'creationTypeID_repeated' => 3, 'refunded' => 89],
+  ['name' => 'VISA Deposit', 'num' => 2, 'num_repeated' => 2, 'creationTypeID' => 1, 'creationTypeID_repeated' => 3, 'refunded' => 88],
+  ['name' => 'Maestro Deposit', 'num' => 73, 'num_repeated' => 73, 'creationTypeID' => 1, 'creationTypeID_repeated' => 1, 'refunded' => 90],
+]);
 
-$payment_method = new Payment_method;
-$payment_method->name = 'VISA Deposit';
-$payment_method->num = '2';
-$payment_method->num_repeated = '2';
-$payment_method->creationTypeID = '1';
-$payment_method->creationTypeID_repeated = '3';
-$payment_method->refunded = '88';
-$payment_method->save();
-
-$payment_method = new Payment_method;
-$payment_method->name = 'Maestro Deposit';
-$payment_method->num = '73';
-$payment_method->num_repeated = '73';
-$payment_method->creationTypeID = '1';
-$payment_method->creationTypeID_repeated = '1';
-$payment_method->refunded = '90';
-$payment_method->save();
+$user = User::create([
+  'username' => 'Yakov', 
+  'firstname' => 'Yakov',
+  'lastname' => 'Litvak',
+  'currencyCode' => 'USD',
+  'languageCode' => 'EN',
+  'email' => 'yakov.litvak@qbex.io',
+  'dateOfBirth' => '1989-09-28T00:00:00',
+  'gender' => 'Male',
+]);
+Address::create([
+  'user_id' => $user->userID,
+  'street' => 'Marxergasse',
+  'houseNumber' => '34b',
+  'postalCode' => '1024',
+  'city' => 'Vienna',
+  'countryCode2' => 'AT',
+  'telephoneNumber' => '0064765475',
+]);
